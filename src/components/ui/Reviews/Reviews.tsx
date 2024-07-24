35eed4b921b69a68fa22d57aecc4496e5ea1eb66
GabrielGiorgis <gabi.giorgis6@gmail.com>
2024-07-24 19:16:58 -0300

    Slider reviews

--- src/components/ui/Reviews/Reviews.tsx
+++ src/components/ui/Reviews/Reviews.tsx
@@ -1,53 +1,59 @@
 import { useState } from 'react';
 import './Reviews.css';
 import { Review } from './Review';
-import { ArrowForwardIos } from '@mui/icons-material';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const reviews = [
     {
         id: 1,
-        name: 'Billie',
-        review: 'Fantastic team leader with a great vision for the project.',
+        name: 'Pablo V.',
+        review: 'Ya van 3 días que lo pruebo, y las energías a full. Lo super recomiendo ¡MUCHAS GRACIAS!',
     },
     {
         id: 2,
-        name: 'Roger',
-        review: 'Creative genius who brings ideas to life beautifully.',
+        name: 'Locuano Acosta',
+        review: 'EXCELENTE! EL sabor es suave... pero rico! Despues probare los otros sabores.',
     },
     {
         id: 3,
-        name: 'Wendy',
-        review: 'Detail-oriented designer with a knack for clean, modern designs.',
+        name: 'Matias L.',
+        review: 'Excelente sabor, lo que hacen estos pibes simplemente es genial.',
     },
 ];
 
 export function Reviews() {
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const [index, setIndex] = useState(0);
 
-    const handleNext = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % reviews.length);
+    const handleSelect = (selectedIndex: number) => {
+        setIndex(selectedIndex);
     };
 
-    const handlePrevious = () => {
-        setCurrentIndex((prevIndex) =>
-            prevIndex === 0 ? reviews.length - 1 : prevIndex - 1
-        );
-    };
 
     return (
-        <div className="carousel">
-            <div className="carousel-button" >
-                <ArrowForwardIos onClick={handlePrevious} style={{ transform: 'rotate(180deg)', cursor: 'pointer' }}/>
-            </div>
-            <div className="carousel-content">
-                <Review
-                    name={reviews[currentIndex].name}
-                    review={reviews[currentIndex].review}
-                />
-            </div>
-            <div className="carousel-button" >
-                <ArrowForwardIos onClick={handleNext} style={{ cursor: 'pointer' }}/>
-            </div>
-        </div>
+        // <div className="carousel">
+        //     <div className="carousel-button" >
+        //         <ArrowForwardIos onClick={handlePrevious} style={{ transform: 'rotate(180deg)', cursor: 'pointer' }}/>
+        //     </div>
+        //     <div className="carousel-content">
+        //         <Review
+        //             name={reviews[currentIndex].name}
+        //             review={reviews[currentIndex].review}
+        //         />
+        //     </div>
+        //     <div className="carousel-button" >
+        //         <ArrowForwardIos onClick={handleNext} style={{ cursor: 'pointer' }}/>
+        //     </div>
+        // </div>
+        <Carousel activeIndex={index} onSelect={handleSelect} controls={false} indicators={false} >
+            {reviews.map((review) => (
+                <Carousel.Item>
+                    <Review
+                        name={review.name}
+                        review={review.review}
+                    />
+                </Carousel.Item>
+            ))}
+        </Carousel>
     );
 }
